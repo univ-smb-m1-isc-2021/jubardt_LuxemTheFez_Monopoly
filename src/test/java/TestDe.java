11adff010a85fe1d2426450f37a2954b592a09a1
LuxemTheFez <37304100+LuxemTheFez@users.noreply.github.com>
2022-01-17 16:13:18 +0100

    Update TestDe.java

--- src/test/java/TestDe.java
+++ src/test/java/TestDe.java
@@ -15,7 +15,7 @@ public class TestDe{
 	@Test
 	public void testverifRes() {
 		De de = new De(6);
-		for(int i=0; i<1000;i++){
+		for(int i=0; i<100;i++){
 			int res = de.lancer();
 			assertTrue(res<=6);
 			assertTrue(res>0);
